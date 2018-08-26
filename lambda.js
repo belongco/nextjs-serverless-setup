c2f5467b7548cf27f1df764d02f9d31141e4c5a5
Vivek Kaarthek <kaarthek@belong.co>
2018-08-26 10:29:39 +0530

    Provide all binaryMimeTypes

--- lambda.js
+++ lambda.js
@@ -2,23 +2,7 @@ const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./server');
 
 const binaryMimeTypes = [
-  'application/javascript',
-  'application/json',
-  'application/octet-stream',
-  'application/xml',
-  'font/eot',
-  'font/opentype',
-  'font/otf',
-  'image/jpeg',
-  'image/png',
-  'image/svg+xml',
-  'text/comma-separated-values',
-  'text/css',
-  'text/html',
-  'text/javascript',
-  'text/plain',
-  'text/text',
-  'text/xml'
+  '*/*'
 ];
 
 const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
